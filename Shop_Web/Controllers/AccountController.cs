b4ac6d362de1b72842fd93d45e75df925ab8fb22
arad_enj <arad_enj@yahoo.com>
2020-05-07 17:45:39 +0430

    Sync

---
+++ Shop_Web/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Shop_Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Shop_Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace Shop_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult Slider()
+        {
+            return PartialView();
+        }
+    }
+}
