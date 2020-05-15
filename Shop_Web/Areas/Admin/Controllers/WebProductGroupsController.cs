476d895e4041af9b2b950c4c981f7fb17de67e28
arad_enj <arad_enj@yahoo.com>
2020-05-15 17:31:10 +0430

    Sync

--- Shop_Web/Areas/Admin/Controllers/WebProductGroupsController.cs
+++ Shop_Web/Areas/Admin/Controllers/WebProductGroupsController.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using EntityCache.Assistence;
+using EntityCache.Bussines;
 using EntityCache.WebBussines;
+using PacketParser.Services;
 
 namespace Shop_Web.Areas.Admin.Controllers
 {
@@ -10,29 +15,38 @@ namespace Shop_Web.Areas.Admin.Controllers
         // GET: Admin/WebProductGroups
         public ActionResult Index()
         {
-            return View(WebProductGroup.GetAll().Where(q=>q.ParentGuid==Guid.Empty).OrderBy(q=>q.Code));
+            return View();
+        }
+
+        public ActionResult LiatGroups()
+        {
+            return PartialView(WebProductGroup.GetAll().Where(q => q.ParentGuid == Guid.Empty).OrderBy(q => q.Code));
         }
 
         // GET: Admin/WebProductGroups/Details/5
         public ActionResult Details(Guid? id)
         {
-            //if (id == null)
-            //{
-            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            //}
-            //WebProductGroup webProductGroup = db.WebProductGroups.Find(id);
-            //if (webProductGroup == null)
-            //{
-            //    return HttpNotFound();
-            //}
-            //return View(webProductGroup);
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var webProductGroup = WebProductGroup.Get(id.Value);
+            if (webProductGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("LiatGroups",
+                WebProductGroup.GetAll().Where(q => q.ParentGuid == Guid.Empty).OrderBy(q => q.Code));
         }
 
         // GET: Admin/WebProductGroups/Create
-        public ActionResult Create()
+        public ActionResult Create(Guid? id)
         {
-            return View();
+            return PartialView(new WebProductGroup()
+            {
+                ParentGuid = id ?? Guid.Empty
+            });
         }
 
         // POST: Admin/WebProductGroups/Create
@@ -40,34 +54,48 @@ namespace Shop_Web.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Guid,Modified,Code,Name,ParentGuid,Description")] WebProductGroup webProductGroup)
+        public async Task<ActionResult> Create([Bind(Include = "Guid,Modified,Code,Name,ParentGuid,Description")] WebProductGroup webProductGroup)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    webProductGroup.Guid = Guid.NewGuid();
-            //    db.WebProductGroups.Add(webProductGroup);
-            //    db.SaveChanges();
-            //    return RedirectToAction("Index");
-            //}
+            try
+            {
+                if (string.IsNullOrEmpty(webProductGroup.Name)) return View(webProductGroup);
 
-            //return View(webProductGroup);
-            return View();
+                var prdGroup = new ProductGroupBussines
+                {
+                    Guid = Guid.NewGuid(),
+                    ParentGuid = webProductGroup.ParentGuid,
+                    Name = webProductGroup.Name,
+                    Modified = DateTime.Now,
+                    Code = await ProductGroupBussines.NextCode(),
+                    Description = ""
+                };
+
+                await prdGroup.SaveAsync();
+                return PartialView("LiatGroups",
+                    WebProductGroup.GetAll().Where(q => q.ParentGuid == Guid.Empty).OrderBy(q => q.Code));
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+
+            return View(webProductGroup);
         }
 
         // GET: Admin/WebProductGroups/Edit/5
         public ActionResult Edit(Guid? id)
         {
-            //if (id == null)
-            //{
-            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            //}
-            //WebProductGroup webProductGroup = db.WebProductGroups.Find(id);
-            //if (webProductGroup == null)
-            //{
-            //    return HttpNotFound();
-            //}
-            //return View(webProductGroup);
-            return View();
+            if (id == null || id == Guid.Empty)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var webProductGroup = WebProductGroup.Get(id.Value);
+            if (webProductGroup == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(webProductGroup);
         }
 
         // POST: Admin/WebProductGroups/Edit/5
@@ -75,32 +103,46 @@ namespace Shop_Web.Areas.Admin.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Guid,Modified,Code,Name,ParentGuid,Description")] WebProductGroup webProductGroup)
+        public async Task<ActionResult> Edit([Bind(Include = "Guid,Modified,Code,Name,ParentGuid,Description")] WebProductGroup webProductGroup)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    db.Entry(webProductGroup).State = EntityState.Modified;
-            //    db.SaveChanges();
-            //    return RedirectToAction("Index");
-            //}
-            //return View(webProductGroup);
-            return View();
+            try
+            {
+                if (string.IsNullOrEmpty(webProductGroup.Name)) return View(webProductGroup);
+                var grp = await ProductGroupBussines.GetAsync(webProductGroup.Guid);
+                if (grp == null) return View(webProductGroup);
+                grp.Name = webProductGroup.Name;
+                await grp.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            return PartialView("LiatGroups",
+                WebProductGroup.GetAll().Where(q => q.ParentGuid == Guid.Empty).OrderBy(q => q.Code));
         }
 
         // GET: Admin/WebProductGroups/Delete/5
-        public ActionResult Delete(Guid? id)
+        public async Task Delete(Guid id)
         {
-            //if (id == null)
-            //{
-            //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            //}
-            //WebProductGroup webProductGroup = db.WebProductGroups.Find(id);
-            //if (webProductGroup == null)
-            //{
-            //    return HttpNotFound();
-            //}
-            //return View(webProductGroup);
-            return View();
+            try
+            {
+                var grp = await ProductGroupBussines.GetAsync(id);
+                if (grp == null) return;
+                if (await ProductGroupBussines.ChildCount(id) > 0)
+                {
+                    var childs = await ProductGroupBussines.GetAllAsync();
+                    childs = childs.Where(q => q.ParentGuid == id).ToList();
+                    foreach (var item in childs)
+                    {
+                        await item.RemoveAsync();
+                    }
+                }
+                await grp.RemoveAsync();
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
         }
 
         // POST: Admin/WebProductGroups/Delete/5
@@ -119,7 +161,7 @@ namespace Shop_Web.Areas.Admin.Controllers
         {
             if (disposing)
             {
-                //db.Dispose();
+                //UnitOfWork.Dispose();
             }
             base.Dispose(disposing);
         }
