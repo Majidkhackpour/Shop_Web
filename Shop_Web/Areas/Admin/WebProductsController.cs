2a6169e7ab625d7390db222a25bf90b91f012c01
arad_enj <arad_enj@yahoo.com>
2020-05-19 21:10:27 +0430

    Sync

--- Shop_Web/Areas/Admin/WebProductsController.cs
+++ Shop_Web/Areas/Admin/WebProductsController.cs
@@ -147,19 +147,23 @@ namespace Shop_Web.Areas.Admin
         }
 
         // GET: Admin/WebProducts/Edit/5
-        public ActionResult Edit(Guid? id)
+        public async Task<ActionResult> Edit(Guid? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //WebProduct webProduct = db.WebProducts.Find(id);
-            //if (webProduct == null)
-            //{
-            //    return HttpNotFound();
-            //}
-            //return View(webProduct);
-            return View();
+
+            var webProduct = WebProduct.Get(id.Value);
+            if (webProduct == null)
+            {
+                return HttpNotFound();
+            }
+            var list = await ProductGroupBussines.GetAllAsync();
+            ViewBag.Groups = list.ToList();
+            ViewBag.SelectedGroups = webProduct.GroupList.ToList();
+            ViewBag.Tags = string.Join(",", webProduct.TagsList.Select(q => q.Tag).ToList());
+            return View(webProduct);
         }
 
         // POST: Admin/WebProducts/Edit/5
@@ -167,16 +171,78 @@ namespace Shop_Web.Areas.Admin
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Guid,Modified,Code,Name,CreateDate,ImageName,Price,Description,ShortDesc,Abad,Kind,Color")] WebProduct webProduct)
+        public async Task<ActionResult> Edit([Bind(Include =
+                "Guid,Modified,Code,Name,CreateDate,ImageName,Price,Description,ShortDesc,Abad,Kind,Color")]
+            WebProduct webProduct, List<Guid> selectedGroups, HttpPostedFileBase imageproduct, string tags)
         {
-            //if (ModelState.IsValid)
-            //{
-            //    db.Entry(webProduct).State = EntityState.Modified;
-            //    db.SaveChanges();
-            //    return RedirectToAction("Index");
-            //}
-            //return View(webProduct);
-            return View();
+            try
+            {
+                if (!ModelState.IsValid) return View(webProduct);
+                var prdoduct = await ProductBussines.GetAsync(webProduct.Guid);
+                if (imageproduct != null && imageproduct.IsImage())
+                {
+                    if (webProduct.ImageName != "No.png")
+                    {
+                        System.IO.File.Delete(Server.MapPath("/Images/ProductImages/" + webProduct.ImageName));
+                        System.IO.File.Delete(Server.MapPath("/Images/ProductImages/Thumb/" + webProduct.ImageName));
+                    }
+                    prdoduct.ImageName = Guid.NewGuid() + Path.GetExtension(imageproduct.FileName);
+                    imageproduct.SaveAs(Server.MapPath("/Images/ProductImages/" + prdoduct.ImageName));
+                    var img = new ImageResizer();
+                    img.Resize(Server.MapPath("/Images/ProductImages/" + prdoduct.ImageName),
+                        Server.MapPath("/Images/ProductImages/Thumb/" + prdoduct.ImageName));
+                }
+                var tag = tags.Split(',');
+                var tagList = new List<PrdTagBussines>();
+                foreach (var t in tag)
+                {
+                    var a = new PrdTagBussines()
+                    {
+                        Guid = Guid.NewGuid(),
+                        Modified = DateTime.Now,
+                        PrdGuid = prdoduct.Guid,
+                        Tag = t.Trim()
+                    };
+                    tagList.Add(a);
+                }
+
+                prdoduct.TagsList = tagList;
+
+                var list = new List<PrdSelectedGroupBussines>();
+                foreach (var item in selectedGroups)
+                {
+                    var a = new PrdSelectedGroupBussines()
+                    {
+                        Guid = Guid.NewGuid(),
+                        Modified = DateTime.Now,
+                        PrdGuid = prdoduct.Guid,
+                        GroupGuid = item
+                    };
+                    list.Add(a);
+                }
+
+                prdoduct.GroupList = list;
+
+
+                prdoduct.Name = webProduct.Name;
+                prdoduct.Description = webProduct.Description.Replace("<p>", "").Replace("</p>", "");
+                prdoduct.Abad = webProduct.Abad;
+                prdoduct.Color = webProduct.Color;
+                prdoduct.Kind = webProduct.Kind;
+                prdoduct.Price = webProduct.Price;
+                prdoduct.ShortDesc = webProduct.ShortDesc;
+
+                await prdoduct.SaveAsync();
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+            var lst = await ProductGroupBussines.GetAllAsync();
+            ViewBag.Groups = lst.ToList();
+            ViewBag.SelectedGroups = webProduct.GroupList.ToList();
+            ViewBag.Tags = string.Join(",", webProduct.TagsList.Select(q => q.Tag).ToList());
+            return RedirectToAction("Index");
         }
 
         // GET: Admin/WebProducts/Delete/5
@@ -186,13 +252,13 @@ namespace Shop_Web.Areas.Admin
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            //WebProduct webProduct = db.WebProducts.Find(id);
-            //if (webProduct == null)
-            //{
-            //    return HttpNotFound();
-            //}
-            //return View(webProduct);
-            return View();
+
+            var webProduct = WebProduct.Get(id.Value);
+            if (webProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(webProduct);
         }
 
         // POST: Admin/WebProducts/Delete/5
@@ -205,6 +271,68 @@ namespace Shop_Web.Areas.Admin
             //db.SaveChanges();
             //return RedirectToAction("Index");
             return View();
+        }
+
+        public async Task<ActionResult> Gallery(Guid id)
+        {
+            var prd = await ProductBussines.GetAsync(id);
+            ViewBag.Gallery = prd.ImageList;
+            return View(new WebProductPictures()
+            {
+                PrdGuid = id
+            });
+        }
+        [HttpPost]
+        public async Task<ActionResult> Gallery(WebProductPictures webPrd, HttpPostedFileBase imageproduct)
+        {
+            var prd = new ProductPicturesBussines();
+            try
+            {
+                if (!ModelState.IsValid) return RedirectToAction("Gallery", new { id = webPrd.Guid });
+                if (imageproduct != null && imageproduct.IsImage())
+                {
+                    prd.Guid = Guid.NewGuid();
+                    prd.Modified = DateTime.Now;
+                    prd.PrdGuid = webPrd.PrdGuid;
+                    prd.Title = webPrd.Title;
+                    prd.ImageName = Guid.NewGuid() + Path.GetExtension(imageproduct.FileName);
+
+                    imageproduct.SaveAs(Server.MapPath("/Images/ProductImages/" + prd.ImageName));
+                    var img = new ImageResizer();
+                    img.Resize(Server.MapPath("/Images/ProductImages/" + prd.ImageName),
+                        Server.MapPath("/Images/ProductImages/Thumb/" + prd.ImageName));
+
+                    await prd.SaveAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+
+            return RedirectToAction("Gallery", new { id = prd.PrdGuid });
+        }
+
+
+        public ActionResult DeleteGallery(Guid id)
+        {
+            var gallery = new WebProductPictures();
+            try
+            { 
+                gallery = WebProductPictures.Get(id);
+
+                System.IO.File.Delete(Server.MapPath("/Images/ProductImages/" + gallery.ImageName));
+                System.IO.File.Delete(Server.MapPath("/Images/ProductImages/Thumb/" + gallery.ImageName));
+
+                gallery.Remove();
+            }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+            }
+
+
+            return RedirectToAction("Gallery", new { id = gallery.PrdGuid });
         }
 
         protected override void Dispose(bool disposing)
