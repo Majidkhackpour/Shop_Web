ca3055efa38cbcb4d11e15df8922edf66f3fcf24
AppleSoft <AppleSoft@DESKTOP-32SUNQC>
2020-07-06 20:49:57 +0430

    sync

---
+++ Shop_Web/Areas/Admin/Controllers/WebOpinionsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using EntityCache.Bussines;
+using EntityCache.WebBussines;
+
+namespace Shop_Web.Areas.Admin.Controllers
+{
+    public class WebOpinionsController : Controller
+    {
+
+        // GET: Admin/WebOpinions
+        public ActionResult Index()
+        {
+            return View(WebOpinion.GetAll().ToList());
+        }
+
+        // GET: Admin/WebOpinions/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var webOpinion = WebOpinion.Get(id.Value);
+            if (webOpinion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(webOpinion);
+        }
+
+        // GET: Admin/WebOpinions/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var webOpinion = WebOpinion.Get(id.Value);
+            if (webOpinion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(webOpinion);
+        }
+
+        // POST: Admin/WebOpinions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            var webOpinion = await OpinionBussines.GetAsync(id);
+            await webOpinion.RemoveAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Shop_Web/Controllers/WebOpinionsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using EntityCache.Bussines;
+using EntityCache.WebBussines;
+using SqlServerPersistence.Model;
+
+namespace Shop_Web.Controllers
+{
+    public class WebOpinionsController : Controller
+    {
+        // GET: WebOpinions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WebOpinions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Guid,Modified,Name,Email,Opinion")] WebOpinion webOpinion)
+        {
+            if (!ModelState.IsValid) return View(webOpinion);
+            var op = new OpinionBussines()
+            {
+                Guid = Guid.NewGuid(),
+                Opinion = webOpinion.Opinion,
+                Email = webOpinion.Email,
+                Name = webOpinion.Name,
+                Modified = DateTime.Now
+            };
+
+            await op.SaveAsync();
+
+            return Redirect("/");
+        }
+    }
+}
